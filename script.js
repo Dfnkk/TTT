4c9f3ab20979a973c3c95e6c5d342fddcc998ed1
Dfnkk <60377182+Dfnkk@users.noreply.github.com>
2020-05-08 15:48:16 -0700

    adding the code not done

--- script.js
+++ script.js
@@ -67,8 +67,81 @@ turnElement.innerHTML = "Player 1's turn";
 let turn1;
 turn1 = true
 
+function changeTurn() {
+  if (turn1 === true) {
+    turn1 = false
+  } else {
+    turn1 = true
+  }
+}
+
 const main = () => {
   if (turn1 === true) {
-
+    sq1.onclick = () => {
+      sq1.style.backgroundColor = 'red';
+    }
+    sq2.onclick = () => {
+      sq2.style.backgroundColor = 'red';
+    }
+    sq3.onclick = () => {
+      sq3.style.backgroundColor = 'red';
+    }
+    
+    
+    sq4.onclick = () => {
+      sq4.style.backgroundColor = 'red';
+    }
+    sq5.onclick = () => {
+      sq5.style.backgroundColor = 'red';
+    }
+    sq6.onclick = () => {
+      sq6.style.backgroundColor = 'red';
+    }
+    
+    sq7.onclick = () => {
+      sq7.style.backgroundColor = 'red';
+    }
+    sq8.onclick = () => {
+      sq8.style.backgroundColor = 'red';
+    }
+    sq9.onclick = () => {
+      sq9.style.backgroundColor = 'red';
+    }
+  } else {
+    sq1.onclick = () => {
+      sq1.style.backgroundColor = 'blue';
+    }
+    sq2.onclick = () => {
+      sq2.style.backgroundColor = 'blue';
+    }
+    sq3.onclick = () => {
+      sq3.style.backgroundColor = 'blue';
+    }
+    
+    
+    sq4.onclick = () => {
+      sq4.style.backgroundColor = 'blue';
+    }
+    sq5.onclick = () => {
+      sq5.style.backgroundColor = 'blue';
+    }
+    sq6.onclick = () => {
+      sq6.style.backgroundColor = 'blue';
+    }
+    
+    sq7.onclick = () => {
+      sq7.style.backgroundColor = 'blue';
+    }
+    sq8.onclick = () => {
+      sq8.style.backgroundColor = 'blue';
+    }
+    sq9.onclick = () => {
+      sq9.style.backgroundColor = 'blue';
+    }
   }
 };
+
+let wl = true
+while (wl === true) {
+  main();
+}
