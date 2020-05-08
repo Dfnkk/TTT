8ecb015e90d62870becd86166c209740afcc8637
Dfnkk <60377182+Dfnkk@users.noreply.github.com>
2020-05-08 14:00:44 -0700

    adding the files

---
+++ script.js
@@ -0,0 +1,51 @@
+/* var doorImage1 = document.getElementById('door1');
+
+doorImage1.onclick = () => {
+    doorImage1.src = botDoorPath;
+}
+
+let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg'; */
+
+let sq1 = document.querySelector('.top-left');
+let sq2 = document.querySelector('.top-middle');
+let sq3 = document.querySelector('.top-right');
+
+
+let sq4 = document.querySelector('.middle-left');
+let sq5 = document.querySelector('.middle-middle');
+let sq6 = document.querySelector('.middle-right');
+
+let sq7 = document.querySelector('.bottom-left');
+let sq8 = document.querySelector('.bottom-middle');
+let sq9 = document.querySelector('.bottom-right');
+
+sq1.onclick = () => {
+  sq1.style.backgroundColor = 'red';
+}
+sq2.onclick = () => {
+  sq2.style.backgroundColor = 'red';
+}
+sq3.onclick = () => {
+  sq3.style.backgroundColor = 'red';
+}
+
+
+sq4.onclick = () => {
+  sq4.style.backgroundColor = 'red';
+}
+sq5.onclick = () => {
+  sq5.style.backgroundColor = 'red';
+}
+sq6.onclick = () => {
+  sq6.style.backgroundColor = 'red';
+}
+
+sq7.onclick = () => {
+  sq7.style.backgroundColor = 'red';
+}
+sq8.onclick = () => {
+  sq8.style.backgroundColor = 'red';
+}
+sq9.onclick = () => {
+  sq9.style.backgroundColor = 'red';
+}
